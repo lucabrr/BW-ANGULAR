d3127bc3946b23c5551ac3aadd4192bbbaa9bffc
lucabrr <lucabrancato95@gmail.com>
2023-06-12 15:28:57 +0200

    inizializzazione

---
+++ epiforum/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'register', loadChildren: () => import('./pages/auth/register/register.module').then(m => m.RegisterModule) },
+ { path: 'login', loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
