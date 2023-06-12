5f00a7c201181d5d62086b09f1e99fab3fe62791
MarLizZo <marketto140@libero.it>
2023-06-12 20:42:19 +0200

    test

--- epiforum/src/app/pages/home/home.component.ts
+++ epiforum/src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+interface IMsgs {
+  id?: number;
+  content: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  messaggi: IMsgs[] = [];
 
+  ngOnInit() {}
+
+  ngOnDestroy() {}
 }
