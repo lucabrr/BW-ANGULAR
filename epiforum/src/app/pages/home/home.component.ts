1da70d3cc60033fd05974ea44741de68b715b4a9
MarLizZo <marketto140@libero.it>
2023-06-12 22:31:31 +0200

    worker working

--- epiforum/src/app/pages/home/home.component.ts
+++ epiforum/src/app/pages/home/home.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 
-interface IMsgs {
-  id?: number;
-  content: string;
-}
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  messaggi: IMsgs[] = [];
+  messaggi: [] = [];
 
-  ngOnInit() {}
+  ngOnInit() {
+    const worker = new Worker('assets/worker.js');
+    worker.postMessage(
+      'https://6486d413beba6297278f3df3.mockapi.io/api/messaggi'
+    );
+    worker.onmessage = (event) => {
+      const data = event.data;
+
+      if (data.error) {
+        console.error(data.error);
+      } else {
+        console.log(data);
+      }
+    };
+  }
 
   ngOnDestroy() {}
 }
