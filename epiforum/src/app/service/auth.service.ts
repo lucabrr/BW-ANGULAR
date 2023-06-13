58ed5e040d646d9e02fcf2fdff1104e631ee8a9f
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-06-13 10:34:40 +0200

    commit presetting

--- epiforum/src/app/service/auth.service.ts
+++ epiforum/src/app/service/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  jwtHelper: JwtHelperService = new JwtHelperService();
+
+  myUrl: string = 'http://localhost:3000/login';
+
+  private authSubject = new BehaviorSubject<null | >(null);
 
   constructor() { }
 }
