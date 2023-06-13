4b5a1679320736a7daabbaf68263981a39c865a0
MarLizZo <marketto140@libero.it>
2023-06-13 16:32:09 +0200

    login + register ok

--- epiforum/src/app/pages/auth/login/login.component.ts
+++ epiforum/src/app/pages/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { IloginUser } from 'src/app/interfaces/IloginUser';
 import { AuthService } from 'src/app/service/auth.service';
 
@@ -15,17 +16,25 @@ export class LoginComponent {
     private modalService: NgbModal,
     private router: Router
   ) {}
+
   data: IloginUser = {
     email: '',
     password: '',
   };
+
   modalTitle: string = 'Titolo';
   modalTitleUser: string = 'User';
   modalContent: string = 'Content';
   @ViewChild('content')
   mymodal!: ElementRef;
+  logSub!: Subscription;
+
+  ngOnDestroy() {
+    if (this.logSub) this.logSub.unsubscribe();
+  }
+
   login() {
-    this.authSvc.login(this.data).subscribe((access) => {
+    this.logSub = this.authSvc.login(this.data).subscribe((access) => {
       this.modalTitle = `Benvenuto, `;
       this.modalTitleUser = access.user.username;
       this.modalContent = 'Sarai reindirizzato alla home in 3 secondi..';
--- epiforum/src/app/pages/auth/register/register.component.ts
+++ epiforum/src/app/pages/auth/register/register.component.ts
@@ -1,9 +1,8 @@
-import { IregisterUser } from './../../../interfaces/IregisterUser';
-
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/service/auth.service';
 import { RegisterService } from 'src/app/service/register.service';
 
@@ -20,6 +19,8 @@ export class RegisterComponent {
   modalContent: string = 'Content';
   @ViewChild('content')
   mymodal!: ElementRef;
+  logSub!: Subscription;
+  regSub!: Subscription;
 
   constructor(
     private regSvc: RegisterService,
@@ -28,14 +29,19 @@ export class RegisterComponent {
     private modalService: NgbModal
   ) {}
 
+  ngOnDestroy() {
+    if (this.logSub) this.logSub.unsubscribe();
+    if (this.regSub) this.regSub.unsubscribe();
+  }
+
   submit(form: NgForm) {
-    this.regSvc.submit(form.value).subscribe((res) => {
+    this.regSub = this.regSvc.submit(form.value).subscribe((res) => {
       console.log(res);
       const obj = {
         email: this.email,
         password: this.password,
       };
-      this.loginSvc.login(obj).subscribe((logged) => {
+      this.logSub = this.loginSvc.login(obj).subscribe((logged) => {
         this.modalTitle = `Benvenuto, `;
         this.modalTitleUser = logged.user.username;
         this.modalContent = 'Sarai reindirizzato alla home in 3 secondi..';
