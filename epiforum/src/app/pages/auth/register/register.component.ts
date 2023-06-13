bd093dfd41d6de620fb1db9b5fea2e7d56ab6620
lucabrr <lucabrancato95@gmail.com>
2023-06-13 14:23:43 +0200

    register

--- epiforum/src/app/pages/auth/register/register.component.ts
+++ epiforum/src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,9 @@
+import { IregisterUser } from './../../../interfaces/IregisterUser';
+
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterService } from 'src/app/service/register.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+password!:string
+confirmPassword!:string
+
+constructor(private regSvc:RegisterService, private router:Router){}
+
+submit(form:NgForm){
+
+  this.regSvc.submit(form.value).subscribe(res => {console.log(res); this.router.navigate(['/login'])}
+  )
+
+
+}
 
 }
