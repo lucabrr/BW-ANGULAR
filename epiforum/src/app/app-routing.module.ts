4e7a48ff66e809e5e42f8fff2958cd9b1161c0bf
lucabrr <lucabrancato95@gmail.com>
2023-06-15 11:45:39 +0200

    logica setting

--- epiforum/src/app/app-routing.module.ts
+++ epiforum/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'register', loadChildren: () => import('./pages/auth/register/register.module').then(m => m.RegisterModule) },
   { path: 'login', loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule) },
-  { path: 'logout', loadChildren: () => import('./pages/auth/logout/logout.module').then(m => m.LogoutModule) }
+  { path: 'logout', loadChildren: () => import('./pages/auth/logout/logout.module').then(m => m.LogoutModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) }
 ];
 
 @NgModule({
---
+++ epiforum/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IauthResponse } from 'src/app/interfaces/IauthResponse';
+import { IloginUser } from 'src/app/interfaces/IloginUser';
+import { ProfileService } from 'src/app/service/profile.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+
+  constructor(private router:Router, private profileSvc:ProfileService){}
+
+  user!:IauthResponse
+
+  ngOnInit(): void {
+    const userJson = localStorage.getItem("user")
+    if(!userJson){
+      this.router.navigate(['/login'])
+    }else
+    {
+      const userParsed = JSON.parse(userJson)
+      this.user = userParsed
+
+
+    }
+
+
+  }
+
+  edit(form:NgForm,){
+    this.profileSvc.editProfile(form,this.user.user.id).subscribe((res) => {console.log(res),
+      this.user.user = res
+      const userStg = JSON.stringify(this.user)
+      localStorage.setItem("user",userStg)
+      this.router.navigate([""])})
+  }
+
+
+
+}
