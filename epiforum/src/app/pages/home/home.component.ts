149a2a978f63f348cb8ee1494f92d05e3ab75112
MarLizZo <marketto140@libero.it>
2023-06-15 12:40:06 +0200

    ngIf logged

--- epiforum/src/app/pages/home/home.component.ts
+++ epiforum/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,21 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   selectedChannel: string = 'chat';
   refreshing: boolean = false;
+  islogged: boolean = false;
+  loggedSub!: Subscription;
+
+  constructor(private svc: AuthService) {}
+
+  ngOnInit() {
+    this.svc.isLoggedIn$.subscribe((res) => {
+      this.islogged = res;
+      console.log(res);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.loggedSub) this.loggedSub.unsubscribe();
+  }
 
   switchChannel(chat: string) {
     this.refreshing = true;
